f8871f8b2556f51e4e5e8b9513a2f2011ecdfdce
Elena Berezina <farfalina1908@gmail.com>
2023-04-27 22:35:16 +0300

    beginned component Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import {Profile} from './Profile/Profile';
-import user from '../data/user';
+import user from '../data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json';
+
 
 export const App = () => {
   return (
@@ -10,17 +13,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
       />
 
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
